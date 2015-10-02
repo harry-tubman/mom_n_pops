718648e4d8660afb3873e57bcc52c2fac55ec613
harry_tubman <jjones@peopleunited.org>
2015-10-02 00:53:30 +0000

    Added Products and Causes to Profile Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @listings = Listing.where(user: @user)
+    @orders = Order.all.where(buyer: @user).order("created_at DESC")
   end
 
 end
